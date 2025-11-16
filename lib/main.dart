5172c643da19c08c05ed9f153ff9bfdcd06f44f1
lewitzki <matozopedro8@gmail.com>
2025-11-16 17:07:31 -0300

    criar funcao de criar, editar e excluir

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,18 @@
 import 'package:controle_de_despesas/home.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: const FirebaseOptions(
+      apiKey: "AIzaSyCXNacmKcMl4Y-BjGtCNFqrrnoy5hgeSmY",
+      appId: "1:396508489432:web:f63dc5b6d96670650a19ad",
+      messagingSenderId: "396508489432",
+      projectId: "controle-de-despesas-6714f",
+    ),
+  );
+
   runApp(const MyApp());
 }
 
@@ -12,10 +23,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Controle de Despesas',
-      theme: ThemeData(
-        colorScheme: .fromSeed(seedColor: Colors.black),
-      ),
-      home: const HomePage()
+      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.black)),
+      home: const HomePage(),
     );
   }
 }
